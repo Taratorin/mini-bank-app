b04a87f67e3e2d88688f394f4dcee6b20c0103ea
taratorin <unknownmanty@yandex.ru>
2026-05-07 16:35:17 +0300

    add notifications for transfer service

---
+++ transfer-service/src/main/java/ru/cloudwithout/transferservice/client/AccountsClient.java
@@ -0,0 +1,46 @@
+package ru.cloudwithout.transferservice.client;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import ru.cloudwithout.transferservice.model.CommonResponse;
+
+import java.net.URI;
+
+import static org.springframework.security.oauth2.client.web.ClientAttributes.clientRegistrationId;
+
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class AccountsClient {
+
+    private final WebClient accountsWebClient;
+
+    @Value("${bank.accounts.base-url}")
+    private String accountsBaseUrl;
+
+    public CommonResponse transfer(String from, int value, String to) {
+        URI uri = UriComponentsBuilder.fromUriString(accountsBaseUrl)
+                .path("/accounts/transfer")
+                .queryParam("from", from)
+                .queryParam("value", value)
+                .queryParam("to", to)
+                .build()
+                .toUri();
+        log.info("Отправляем запрос на перевод денежных средств: от={}, сумма={}, к={}, адрес={}",
+                from, value, to, uri);
+        CommonResponse response = accountsWebClient
+                .post()
+                .uri(uri)
+                .attributes(clientRegistrationId("transfer-service"))
+                .retrieve()
+                .bodyToMono(CommonResponse.class)
+                .block();
+        log.info("Получили ответ по операции со счетом пользователя {}", from);
+        return response;
+    }
+}
---
+++ transfer-service/src/main/java/ru/cloudwithout/transferservice/config/WebClientConfig.java
@@ -0,0 +1,44 @@
+package ru.cloudwithout.transferservice.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@Slf4j
+public class WebClientConfig {
+
+    @Bean
+    public WebClient accountsWebClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+
+        return WebClient.builder()
+                .apply(oauth2.oauth2Configuration())
+                .build();
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+
+        DefaultOAuth2AuthorizedClientManager manager = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, authorizedClientRepository);
+        manager.setAuthorizedClientProvider(provider);
+
+        return manager;
+    }
+}
