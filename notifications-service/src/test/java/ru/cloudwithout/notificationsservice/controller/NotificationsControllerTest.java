438f1f965fc35bf7b0e559494a49d32faaff1c89
taratorin <unknownmanty@yandex.ru>
2026-05-07 16:39:25 +0300

    add notification service tests

---
+++ notifications-service/src/test/java/ru/cloudwithout/notificationsservice/controller/NotificationsControllerTest.java
@@ -0,0 +1,50 @@
+package ru.cloudwithout.notificationsservice.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class NotificationsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(roles = "SERVICE")
+    void notifyShouldAcceptRequestFromServiceRole() throws Exception {
+        mockMvc.perform(post("/notifications")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "service":"cash-service",
+                                  "operation":"cash-put",
+                                  "message":"ok"
+                                }
+                                """))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(roles = "SERVICE")
+    void notifyShouldAcceptRequestInControllerTestWithoutFilters() throws Exception {
+        mockMvc.perform(post("/notifications")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "service":"cash-service",
+                                  "operation":"cash-put",
+                                  "message":"ok"
+                                }
+                                """))
+                .andExpect(status().isOk());
+    }
+}
