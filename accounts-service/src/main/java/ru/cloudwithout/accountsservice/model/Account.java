a4b04191733c0406e0e69e13b69e15b742774fe2
taratorin <unknownmanty@yandex.ru>
2026-05-07 16:32:43 +0300

    add model classes

--- accounts-service/src/main/java/ru/cloudwithout/accountsservice/model/Account.java
+++ accounts-service/src/main/java/ru/cloudwithout/accountsservice/model/Account.java
@@ -1,13 +1,11 @@
 package ru.cloudwithout.accountsservice.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
 
 @Entity
 @Table(name = "accounts")
