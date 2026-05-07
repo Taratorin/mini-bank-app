8c58af647140c9ec67f6b739935083fc857d883b
taratorin <unknownmanty@yandex.ru>
2026-05-07 16:40:06 +0300

    add contract test description

---
+++ accounts-service/src/contractTest/resources/contracts/accounts/put_cash.groovy
@@ -0,0 +1,32 @@
+package contracts.accounts
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description "Should put cash to account"
+    request {
+        method POST()
+        urlPath("/accounts/cash") {
+            queryParameters {
+                parameter "login": "test"
+                parameter "value": "100"
+                parameter "action": "PUT"
+            }
+        }
+    }
+    response {
+        status OK()
+        headers {
+            contentType(applicationJson())
+        }
+        body(
+                login: "test",
+                firstLastName: "Иван Иванович",
+                birthDate: "1990-01-01",
+                sum: 200.00,
+                accounts: [],
+                errors: [],
+                info: "Положено руб.: 100"
+        )
+    }
+}
