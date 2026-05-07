8a10ec9c96577a3589deee9d47e5131d085b57f2
taratorin <unknownmanty@yandex.ru>
2026-05-07 16:37:30 +0300

    add MainPageModelFiller

---
+++ bank-ui/src/main/java/ru/cloudwithout/bankui/model/MainPageModelFiller.java
@@ -0,0 +1,27 @@
+package ru.cloudwithout.bankui.model;
+
+import jakarta.annotation.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
+@Component
+public class MainPageModelFiller {
+
+    public void fillModel(
+            Model model,
+            @Nullable CommonResponse commonResponse,
+            @Nullable List<String> errors,
+            @Nullable String info
+    ) {
+        if (commonResponse != null) {
+            model.addAttribute("name", commonResponse.getFirstLastName());
+            model.addAttribute("birthdate", commonResponse.getBirthDate());
+            model.addAttribute("sum", commonResponse.getSum());
+            model.addAttribute("accounts", commonResponse.getAccounts());
+        }
+        model.addAttribute("errors", errors);
+        model.addAttribute("info", info);
+    }
+}
